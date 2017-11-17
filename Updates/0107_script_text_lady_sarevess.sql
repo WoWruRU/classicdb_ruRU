16f99cf4c0682b35396e00c8bb14fedea95abe5a
M2us <Dead_Maus@mail.ru>
2017-11-17 20:39:13 +0500

    [0107] Fixed creature_ai_texts id -198 script_texts id -1000399 Closes #128

---
+++ Updates/0107_script_text_lady_sarevess.sql
@@ -0,0 +1,7 @@
+UPDATE `creature_ai_texts` SET 
+	`content_loc8`='Вам здесь не место! Убить их!' 
+WHERE `entry` = -198;
+
+UPDATE `script_texts` SET 
+	`content_loc8`='Мир – это всего лишь мимолетный сон. Пусть правит КОШМАР!' 
+WHERE `entry` = -1000399;
