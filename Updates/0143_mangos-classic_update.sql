8d6959c628b223a5ba44a633aab49787fc922133
M2us <Dead_Maus@mail.ru>
2019-07-07 16:15:07 +0300

    [0143] Added/Updated text from mangos-classic db

---
+++ Updates/0143_mangos-classic_update.sql
@@ -0,0 +1,318 @@
+-- 2405_love_in_air_darna_sentinels.sql
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 10265) AND (`id` = 11);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(10265, 11, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12866) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12866, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+
+-- 2405_love_in_air_ironforge_guards.sql
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 2121) AND (`id` = 13);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(2121, 13, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12865) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12865, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+
+-- 2405_love_in_air_orgrimmar_grunts.sql
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12870) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12870, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12871) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12871, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 1951) AND (`id` = 14);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(1951, 14, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 1951) AND (`id` = 15);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(1951, 15, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+
+-- 2405_love_in_air_stormwind_guards.sql
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12864) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12864, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 435) AND (`id` = 16);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(435, 16, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 435) AND (`id` = 17);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(435, 17, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+
+-- 2405_love_in_air_undercity_guardians.sql
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 2849) AND (`id` = 14);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(2849, 14, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12861) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12861, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12862) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12862, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+
+-- 2409_civilian_gossip_templates.sql
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12850) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12850, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12850) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12850, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12851) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12851, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12851) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12851, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12852) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12852, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12852) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12852, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12853) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12853, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12853) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12853, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12854) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12854, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12854) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12854, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12855) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12855, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12855) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12855, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 681) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(681, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 682) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(682, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 685) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(685, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 686) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(686, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 691) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(691, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 692) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(692, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 693) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(693, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 694) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(694, 0, 'Я хочу посмотреть на ваши товары.');
+
+UPDATE `locales_npc_text` SET 
+	`Text0_0_loc8`='Не позволяй людям с базара задурить тебе голову, $Gпарень:девица; – с построенным этими гномами туннелем перевозить грузы из Стальгорна куда проще, чем тебе могло бы казаться. Если они попытаются прикрутить к цене налоги за ввоз, просто скажи им, что лучше будешь у меня снаряжение покупать.', 
+	`Text0_1_loc8`='Не позволяй людям с базара задурить тебе голову, $Gпарень:девица; – с построенным этими гномами туннелем перевозить грузы из Стальгорна куда проще, чем тебе могло бы казаться. Если они попытаются прикрутить к цене налоги за ввоз, просто скажи им, что лучше будешь у меня снаряжение покупать.' 
+WHERE `entry` = 1244;
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 694) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(694, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+UPDATE `locales_npc_text` SET 
+	`Text0_0_loc8`='Если вставить панель А в гнездо Б, то, разумеется, оно РВАНЕТ! Вот, дайте я покажу вам, что у меня есть, а потом вы сможете спросить Лилиама о том, как состыковать эти предметы, чтобы они не взорвались.', 
+	`Text0_1_loc8`='Если вставить панель А в гнездо Б, то, разумеется, оно РВАНЕТ! Вот, дайте я покажу вам, что у меня есть, а потом вы сможете спросить Лилиама о том, как состыковать эти предметы, чтобы они не взорвались.' 
+WHERE `entry` = 1249;
+
+DELETE FROM `locales_npc_text` WHERE `entry` = 1248;
+INSERT INTO `locales_npc_text` (`entry`, `Text0_0_loc8`, `Text0_1_loc8`) VALUES 
+(1248, 'Если вставить панель А в гнездо Б, то, разумеется, оно РВАНЕТ! Вот, дайте я покажу вам, что у меня есть, а потом вы сможете спросить Лилиама о том, как состыковать эти предметы, чтобы они не взорвались.', 'Если вставить панель А в гнездо Б, то, разумеется, оно РВАНЕТ! Вот, дайте я покажу вам, что у меня есть, а потом вы сможете спросить Лилиама о том, как состыковать эти предметы, чтобы они не взорвались.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 697) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(697, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 698) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(698, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 688) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(688, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 689) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(689, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 689) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(689, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12856) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12856, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12856) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12856, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12857) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12857, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12857) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12857, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12858) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12858, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12858) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12858, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12859) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12859, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12859) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12859, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12868) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12868, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12868) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12868, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12869) AND (`id` = 0);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12869, 0, 'Я хочу посмотреть на ваши товары.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12869) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12869, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12863) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12863, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12872) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12872, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12873) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12873, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12874) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12874, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12875) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12875, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+
+-- 2409_orgrimmar_civilians_gossip.sql
+DELETE FROM `locales_npc_text` WHERE `entry` = 8310;
+INSERT INTO `locales_npc_text` (`entry`, `Text0_0_loc8`, `Text0_1_loc8`) VALUES 
+(8310, 'Много лет прошло с тех пор, как все мои размышления исходили с самого сердца, но теперь оно холодно словно камень.', 'Много лет прошло с тех пор, как все мои размышления исходили с самого сердца, но теперь оно холодно словно камень.');
+
+DELETE FROM `locales_npc_text` WHERE `entry` = 8311;
+INSERT INTO `locales_npc_text` (`entry`, `Text0_0_loc8`, `Text0_1_loc8`) VALUES 
+(8311, 'После стольких лет сражений, все начали влюбляться друг в друга!', 'После стольких лет сражений, все начали влюбляться друг в друга!');
+
+
+-- 2413_quest_texts.sql
+UPDATE `locales_quest` SET 
+	`RequestItemsText_loc8`='За чертеж имперского зерцала, я возьму шестьдесят слитков тория.$B$BА, это снова ты? Собираешься бежать со слезами на глазах к своим Богам спрашивая их почему они покинули тебя? Выше нос, $C! Никто не говорил, что жизнь справедлива.' 
+WHERE `entry` = 7656;
+
+UPDATE `locales_quest` SET 
+	`OfferRewardText_loc8`='Скипетр Зыбучих Песков снова цел, $N.$B$BЭто $Gдолжен:должна; быть ты, кто использует скипетр. Прежде чем отправиться в Силитус и ударить в гонг, тебе нужно дождаться армии Орды и Альянса.' 
+WHERE `entry` = 8742;
+
+
+-- 2437_morin_cloudstalker.sql
+DELETE FROM `locales_questgiver_greeting` WHERE (`Entry` = 2988) AND (`Type` = 0);
+INSERT INTO `locales_questgiver_greeting` (`Entry`, `Type`, `Text_loc8`) VALUES 
+(2988, 0, 'Долг гонцов – обеспечивать безопасность путешественников на равнинах Мулгора. Те же кто будет угрожать безопасности землям тауренов, рискуют получить наказание от наших рук.');
+
+
+-- ACID [2436][TBCDB][ACID] Lokholar the Ice Lord 13256
+UPDATE `creature_ai_texts` SET 
+	`content_loc8`='КТО СМЕЕТ ПРИЗЫВАТЬ ЛОКОЛАРА? Я пролью кровь тысячи солдат Альянса... никто не устоит против мощи Владыки Льда!' 
+WHERE `entry` = -1136;
+
+UPDATE `creature_ai_texts` SET 
+	`content_loc8`='Я упиваюсь твоим страданием, смертное существо. Пусть сущность твоя застынет вместе с Локоларом!' 
+WHERE `entry` = -1137;
+
+UPDATE `creature_ai_texts` SET 
+	`content_loc8`='Злые, ужасные смертные! Леса плачут. Элементали в ужасе от разрушений. Ивус должен очистить этот мир от вас!'
+WHERE `entry` = -1138;
+
+UPDATE `creature_ai_texts` SET 
+	`content_loc8`='Леса плачут. Элементали в ужасе от разрушений. Ивус должен очистить этот мир от вас!' 
+WHERE `entry` = -1139;
+
+
+-- Other
+DELETE FROM `locales_creature` WHERE `entry` = 10820;
+INSERT INTO `locales_creature` (`entry`, `name_loc8`) VALUES 
+(10820, 'Герцог Беспощадный');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 721) AND (`id` = 11);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(721, 11, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 721) AND (`id` = 12);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(721, 12, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_gossip_menu_option` WHERE (`menu_id` = 12867) AND (`id` = 1);
+INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc8`) VALUES 
+(12867, 1, 'Я $Gхотел:хотела; бы дать вам этот знак любви.');
+
+DELETE FROM `locales_npc_text` WHERE `entry` = 8309;
+INSERT INTO `locales_npc_text` (`entry`, `Text0_0_loc8`, `Text0_1_loc8`) VALUES 
+(8309, 'Похоже любовь витает в воздухе. Надеюсь, меня он не обойдет стороной.', 'Похоже любовь витает в воздухе. Надеюсь, меня он не обойдет стороной.')
