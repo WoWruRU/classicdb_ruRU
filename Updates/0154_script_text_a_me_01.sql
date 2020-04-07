71b10c7e52e31490832536cd04e5a716d1b5eafa
M2us <Dead_Maus@mail.ru>
2020-04-07 20:48:54 +0300

    [0154] Fixed A-Me 01 script text

---
+++ Updates/0154_script_text_a_me_01.sql
@@ -0,0 +1,23 @@
+UPDATE `script_texts` SET 
+	`content_loc8`='Чи-Та хорошо. Все хорошо, Чи-Та. Идти... Идти за Чи-Та. Домой. Чи-Та идти домой.'
+WHERE `entry` = -1000446;
+
+UPDATE `script_texts` SET 
+	`content_loc8`='Хорошо... хорошо, Чи-Та. Чи-Та хорошо... Дом. Ищи дом.'
+WHERE `entry` = -1000447;
+
+UPDATE `script_texts` SET 
+	`content_loc8`='Чи-Та дома! Чи-Та, хорошо! Хорошо Чи-Та. Дом. Дом. Дом.'
+WHERE `entry` = -1000448;
+
+UPDATE `script_texts` SET
+	`content_loc8`='$C хочет обидеть Чи-Та. Чи-Та хорошо.'
+WHERE `entry` = -1000449;
+
+UPDATE `script_texts` SET 
+	`content_loc8`='Опасность! Опасность! $C хочет обидеть Чи-Та.'
+WHERE `entry` = -1000450;
+
+UPDATE `script_texts` SET
+	`content_loc8`='Плохо, $C. $C, плохо!'
+WHERE `entry` = -1000451;
